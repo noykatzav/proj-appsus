0b784a750ccc6e297769452d9acac3cfd8951ab0
Noy Katzav <noykatzav@gmail.com>
2026-08-15 00:14:07 +0300

    Fix category layout

--- apps/mail/cmps/MailMenu.jsx
+++ apps/mail/cmps/MailMenu.jsx
@@ -11,11 +11,12 @@ export function MailMenu({ mails }) {
 
 
     return <section className="mail-menu">
-            <div className="category chosen">
-                <Link to="/mail">
-                    <i className="fa-solid fa-inbox"></i>
-                    <span>Inbox (<span>{mailCnt}</span>)</span>
-                </Link>
-            </div>
+            <Link to="/mail">
+                <div className="category chosen">
+                        <i className="fa-solid fa-inbox"></i>
+                        <span>Inbox</span>
+                        <span>{mailCnt}</span>
+                </div>
+            </Link>
         </section>
 }
